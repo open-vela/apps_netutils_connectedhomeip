e336bcc4bf02209b95b5db180ceef9dac7696bce
liujinye <liujinye@xiaomi.com>
2020-03-23 12:55:09 +0800

    Merge pull request #90 from bhaskar-apple/patch-1
    Update BUILDING.md

--- src/setup_payload/tests/tests.cpp
+++ src/setup_payload/tests/tests.cpp
@@ -29,10 +29,9 @@
 using namespace chip;
 using namespace std;
 
-#define EXPECT_EQ(x, y) ((x) != (y)) ? cerr << __FILE__ << ":" << __LINE__ << ":error EXPECT_EQ(" << x << ", " << y << ")\n", 1 : 0
-
-int testPayloadByteArrayRep()
+void testPayloadByteArrayRep()
 {
+    printf("---Running Test--- %s\n", __FUNCTION__);
     SetupPayload payload;
 
     payload.version               = 5;
@@ -44,14 +43,16 @@ int testPayloadByteArrayRep()
     payload.setUpPINCode          = 2048;
 
     QRCodeSetupPayloadGenerator generator(payload);
-    string result   = generator.payloadBinaryRepresentation();
-    string expected = "000000000000000000100000000000010000000000000000100000000000000000010000000000000011001010";
+    string result         = generator.payloadBinaryRepresentation();
+    string expectedResult = "101000000000000110000000000000000010000000011000000000000000000000010000"
+                            "00000000";
 
-    return EXPECT_EQ(result, expected);
+    assert(result.compare(expectedResult) == 0);
 }
 
-int testPayloadBase45Rep()
+void testPayloadBase45Rep()
 {
+    printf("---Running Test--- %s\n", __FUNCTION__);
     SetupPayload payload;
 
     payload.version               = 5;
@@ -63,72 +64,30 @@ int testPayloadBase45Rep()
     payload.setUpPINCode          = 2048;
 
     QRCodeSetupPayloadGenerator generator(payload);
-    string result   = generator.payloadBase45Representation();
-    string expected = "B20800G00G8G000";
+    string result         = generator.payloadBase45Representation();
+    string expectedResult = "KABG8842Q000211";
 
-    return EXPECT_EQ(result, expected);
+    assert(result.compare(expectedResult) == 0);
 }
 
-int testBase45()
+void testBase45Encoding()
 {
-    int surprises = 0;
-
-    uint8_t input[] = { 10, 10, 10 };
-
-    surprises += EXPECT_EQ(base45Encode(input, 0), "");
-
-    surprises += EXPECT_EQ(base45Encode(input, 1), "A0");
-
-    surprises += EXPECT_EQ(base45Encode(input, 2), "5C1");
-
-    surprises += EXPECT_EQ(base45Encode(input, 3), "5C1A0");
-
-    surprises += EXPECT_EQ(base45Encode((uint8_t *) "Hello World!", sizeof("Hello World!") - 1), "8 C VDN44I3E.VD/94");
-
-    vector<uint8_t> decoded = base45Decode("8 C VDN44I3E.VD/94");
-    string hello_world;
-    for (int _ = 0; _ < decoded.size(); _++)
-    {
-        hello_world += (char) decoded[_];
-    }
-    surprises += EXPECT_EQ(hello_world, "Hello World!");
-
-    // empty == empty
-    surprises += EXPECT_EQ(base45Decode("").size(), 0);
-    // too short
-    surprises += EXPECT_EQ(base45Decode("A").size(), 0);
-    // outside valid chars
-    surprises += EXPECT_EQ(base45Decode("0\x1").size(), 0);
-    surprises += EXPECT_EQ(base45Decode("\x10").size(), 0);
-    surprises += EXPECT_EQ(base45Decode("[0").size(), 0);
-    surprises += EXPECT_EQ(base45Decode("0[").size(), 0);
-
-    // BOGUS chars
-    surprises += EXPECT_EQ(base45Decode("!0").size(), 0);
-    surprises += EXPECT_EQ(base45Decode("\"0").size(), 0);
-    surprises += EXPECT_EQ(base45Decode("#0").size(), 0);
-    surprises += EXPECT_EQ(base45Decode("&0").size(), 0);
-    surprises += EXPECT_EQ(base45Decode("'0").size(), 0);
-    surprises += EXPECT_EQ(base45Decode("(0").size(), 0);
-    surprises += EXPECT_EQ(base45Decode(")0").size(), 0);
-    surprises += EXPECT_EQ(base45Decode(",0").size(), 0);
-    surprises += EXPECT_EQ(base45Decode(";0").size(), 0);
-    surprises += EXPECT_EQ(base45Decode("<0").size(), 0);
-    surprises += EXPECT_EQ(base45Decode("=0").size(), 0);
-    surprises += EXPECT_EQ(base45Decode(">0").size(), 0);
-    surprises += EXPECT_EQ(base45Decode("@0").size(), 0);
-
-    return surprises;
+    printf("---Running Test--- %s\n", __FUNCTION__);
+    uint16_t input        = 10;
+    string result         = base45EncodedString(input, 3);
+    string expectedResult = "00A";
+    assert(result.compare(expectedResult) == 0);
 }
 
-int testBitsetLen()
+void testBitsetLen()
 {
-    return EXPECT_EQ(kTotalPayloadDataSizeInBits % 8, 0);
+    printf("---Running Test--- %s\n", __FUNCTION__);
+    assert(kTotalPayloadDataSizeInBits % 8 == 0);
 }
 
-int testSetupPayloadVerify()
+void testSetupPayloadVerify()
 {
-    int surprises = 0;
+    printf("---Running Test--- %s\n", __FUNCTION__);
     SetupPayload payload;
 
     payload.version               = 5;
@@ -138,33 +97,36 @@ int testSetupPayloadVerify()
     payload.rendezvousInformation = 1;
     payload.discriminator         = 128;
     payload.setUpPINCode          = 2048;
-    surprises += EXPECT_EQ(payload.isValid(), true);
+
+    assert(payload.isValid());
 
     // test invalid version
     SetupPayload test_payload = payload;
     test_payload.version      = 2 << kVersionFieldLengthInBits;
-    surprises += EXPECT_EQ(test_payload.isValid(), false);
+    assert(!test_payload.isValid());
 
     // test invalid rendezvousInformation
     test_payload                       = payload;
     test_payload.rendezvousInformation = 3;
-    surprises += EXPECT_EQ(test_payload.isValid(), false);
+    assert(!test_payload.isValid());
 
     // test invalid discriminator
     test_payload               = payload;
     test_payload.discriminator = 2 << kPayloadDiscriminatorFieldLengthInBits;
-    surprises += EXPECT_EQ(test_payload.isValid(), false);
+    assert(!test_payload.isValid());
 
     // test invalid stetup PIN
     test_payload              = payload;
     test_payload.setUpPINCode = 2 << kSetupPINCodeFieldLengthInBits;
-    surprises += EXPECT_EQ(test_payload.isValid(), false);
-
-    return surprises;
+    assert(!test_payload.isValid());
 }
 
 int main(int argc, char ** argv)
 {
     printf("---Running Test--- tests from %s\n", __FILE__);
-    return testBitsetLen() + testPayloadByteArrayRep() + testPayloadBase45Rep() + testBase45() + testSetupPayloadVerify();
+    testBitsetLen();
+    testPayloadByteArrayRep();
+    testPayloadBase45Rep();
+    testBase45Encoding();
+    testSetupPayloadVerify();
 }
