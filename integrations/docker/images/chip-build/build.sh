a04f929481aa9c899dd206cb820cb2de7952b769
liujinye <liujinye@xiaomi.com>
2020-04-03 14:48:51 +0800

    Remove unused method

--- integrations/docker/images/chip-build/build.sh
+++ integrations/docker/images/chip-build/build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+set -ex
 
-me=$(basename "$0")
 cd "$(dirname "$0")"
+
+die() {
+    echo " *** ERROR: $*"
+    exit 1
+}
 
 # FIX ME, should be "projectchip" or "zigbee" or something,
 #   (docker.io doesn't allow hyphens)
@@ -12,28 +17,6 @@ IMAGE=${DOCKER_BUILD_IMAGE:-$(basename "$(pwd)")}
 
 # version
 VERSION=${DOCKER_BUILD_VERSION:-$(cat version)}
-
-[[ ${*/--help//} != "${*}" ]] && {
-  set +x
-  echo "Usage: $me <OPTIONS>
-
-  Build and (optionally tag as latest, push) a docker image from Dockerfile in CWD
-
-  Options:
-   --latest   update latest to the current built version (\"$VERSION\")
-   --push     push image(s) to docker.io (requires docker login for \"$ORG\")
-   --help     get this message
-
-"
-  exit 0
-}
-
-die() {
-    echo "$me: *** ERROR: $*"
-    exit 1
-}
-
-set -ex
 
 [[ -n $VERSION ]] || die "version cannot be empty"
 
