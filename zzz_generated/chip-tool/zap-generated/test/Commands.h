542ffecba53601b14971c2fff7ee5dd651aac7fa
Michael Spang <spang@google.com>
2022-08-17 10:42:47 -0400

    Add missing includes (#21913)

--- zzz_generated/chip-tool/zap-generated/test/Commands.h
+++ zzz_generated/chip-tool/zap-generated/test/Commands.h
@@ -21,6 +21,7 @@
 
 #if CONFIG_ENABLE_YAML_TESTS
 
+#include <commands/common/Commands.h>
 #include <commands/tests/TestCommand.h>
 #include <lib/core/Optional.h>
 #include <lib/support/CHIPListUtils.h>
