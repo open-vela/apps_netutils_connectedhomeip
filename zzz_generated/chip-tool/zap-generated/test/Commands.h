d153576815dbf2d1ffd26a89f798aab342ea2ff3
Boris Zbarsky <bzbarsky@apple.com>
2023-01-03 17:34:16 -0500

    Align naming in Descriptor cluster XML with the spec. (#24238)
    * Align naming in Descriptor cluster XML with the spec.
    
    * Regenerate generated files.

--- zzz_generated/chip-tool/zap-generated/test/Commands.h
+++ zzz_generated/chip-tool/zap-generated/test/Commands.h
@@ -61122,7 +61122,7 @@ private:
                 {
                     auto iter_0 = value.begin();
                     VerifyOrReturn(CheckNextListItemDecodes<decltype(value)>("deviceTypeList", iter_0, 0));
-                    VerifyOrReturn(CheckValue("deviceTypeList[0].type", iter_0.GetValue().type, 22UL));
+                    VerifyOrReturn(CheckValue("deviceTypeList[0].deviceType", iter_0.GetValue().deviceType, 22UL));
                     VerifyOrReturn(CheckValue("deviceTypeList[0].revision", iter_0.GetValue().revision, 1U));
                     VerifyOrReturn(CheckNoMoreListItems<decltype(value)>("deviceTypeList", iter_0, 1));
                 }
