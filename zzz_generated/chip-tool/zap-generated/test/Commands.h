73b71e557cf1412401ff769e5a8180834b2b327f
Boris Zbarsky <bzbarsky@apple.com>
2023-05-05 14:17:06 -0400

    Turn off the Basic Information Reachable attribute in examples. (#26331)
    This is really only meant to be used for Bridged Device Basic Information, not
    the Basic Information cluster.
    
    Fixes https://github.com/project-chip/connectedhomeip/issues/18083

--- zzz_generated/chip-tool/zap-generated/test/Commands.h
+++ zzz_generated/chip-tool/zap-generated/test/Commands.h
@@ -68939,24 +68939,22 @@ private:
                     VerifyOrReturn(CheckNextListItemDecodes<decltype(value)>("attributeList", iter_0, 16));
                     VerifyOrReturn(CheckValue("attributeList[16]", iter_0.GetValue(), 16UL));
                     VerifyOrReturn(CheckNextListItemDecodes<decltype(value)>("attributeList", iter_0, 17));
-                    VerifyOrReturn(CheckValue("attributeList[17]", iter_0.GetValue(), 17UL));
+                    VerifyOrReturn(CheckValue("attributeList[17]", iter_0.GetValue(), 18UL));
                     VerifyOrReturn(CheckNextListItemDecodes<decltype(value)>("attributeList", iter_0, 18));
-                    VerifyOrReturn(CheckValue("attributeList[18]", iter_0.GetValue(), 18UL));
+                    VerifyOrReturn(CheckValue("attributeList[18]", iter_0.GetValue(), 19UL));
                     VerifyOrReturn(CheckNextListItemDecodes<decltype(value)>("attributeList", iter_0, 19));
-                    VerifyOrReturn(CheckValue("attributeList[19]", iter_0.GetValue(), 19UL));
+                    VerifyOrReturn(CheckValue("attributeList[19]", iter_0.GetValue(), 20UL));
                     VerifyOrReturn(CheckNextListItemDecodes<decltype(value)>("attributeList", iter_0, 20));
-                    VerifyOrReturn(CheckValue("attributeList[20]", iter_0.GetValue(), 20UL));
+                    VerifyOrReturn(CheckValue("attributeList[20]", iter_0.GetValue(), 65528UL));
                     VerifyOrReturn(CheckNextListItemDecodes<decltype(value)>("attributeList", iter_0, 21));
-                    VerifyOrReturn(CheckValue("attributeList[21]", iter_0.GetValue(), 65528UL));
+                    VerifyOrReturn(CheckValue("attributeList[21]", iter_0.GetValue(), 65529UL));
                     VerifyOrReturn(CheckNextListItemDecodes<decltype(value)>("attributeList", iter_0, 22));
-                    VerifyOrReturn(CheckValue("attributeList[22]", iter_0.GetValue(), 65529UL));
+                    VerifyOrReturn(CheckValue("attributeList[22]", iter_0.GetValue(), 65531UL));
                     VerifyOrReturn(CheckNextListItemDecodes<decltype(value)>("attributeList", iter_0, 23));
-                    VerifyOrReturn(CheckValue("attributeList[23]", iter_0.GetValue(), 65531UL));
+                    VerifyOrReturn(CheckValue("attributeList[23]", iter_0.GetValue(), 65532UL));
                     VerifyOrReturn(CheckNextListItemDecodes<decltype(value)>("attributeList", iter_0, 24));
-                    VerifyOrReturn(CheckValue("attributeList[24]", iter_0.GetValue(), 65532UL));
-                    VerifyOrReturn(CheckNextListItemDecodes<decltype(value)>("attributeList", iter_0, 25));
-                    VerifyOrReturn(CheckValue("attributeList[25]", iter_0.GetValue(), 65533UL));
-                    VerifyOrReturn(CheckNoMoreListItems<decltype(value)>("attributeList", iter_0, 26));
+                    VerifyOrReturn(CheckValue("attributeList[24]", iter_0.GetValue(), 65533UL));
+                    VerifyOrReturn(CheckNoMoreListItems<decltype(value)>("attributeList", iter_0, 25));
                 }
             }
             break;
