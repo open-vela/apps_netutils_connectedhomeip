19852042e8d52ce8df9dfb6dd7007da944f310c8
Marius Tache <102153746+marius-alex-tache@users.noreply.github.com>
2023-09-21 15:35:00 +0300

    [data-model] Add general diagnostics average wear count (#29285)
    * [clusters] Add AverageWearCount attribute support in GeneralDiagnostics
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    * [codegen] Regenerate generated code
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    * Add AverageWearCount to zcl data
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    * Remove AverageWearCount from Darwin availability
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    * Add API documentation for General Diagnostics interface
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    * Restyled by whitespace
    
    * Restyled by prettier-json
    
    * Remove redundant comments from General Diagnostics interface
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    * [codegen] Regenerate generated code
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    * Remove redundant min/max range for General Diagnostics attributes
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    
    ---------
    
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/darwin/Framework/CHIP/zap-generated/MTRBaseClusters.h
+++ src/darwin/Framework/CHIP/zap-generated/MTRBaseClusters.h
@@ -4880,6 +4880,18 @@ API_AVAILABLE(ios(16.1), macos(13.0), watchos(9.1), tvos(16.1))
                                                                        NSError * _Nullable error))completion
     API_AVAILABLE(ios(16.4), macos(13.3), watchos(9.4), tvos(16.4));
 
+- (void)readAttributeAverageWearCountWithCompletion:(void (^)(NSNumber * _Nullable value, NSError * _Nullable error))completion
+    MTR_NEWLY_AVAILABLE;
+- (void)subscribeAttributeAverageWearCountWithParams:(MTRSubscribeParams *)params
+                             subscriptionEstablished:(MTRSubscriptionEstablishedHandler _Nullable)subscriptionEstablished
+                                       reportHandler:(void (^)(NSNumber * _Nullable value, NSError * _Nullable error))reportHandler
+    MTR_NEWLY_AVAILABLE;
++ (void)readAttributeAverageWearCountWithClusterStateCache:(MTRClusterStateCacheContainer *)clusterStateCacheContainer
+                                                  endpoint:(NSNumber *)endpoint
+                                                     queue:(dispatch_queue_t)queue
+                                                completion:(void (^)(NSNumber * _Nullable value,
+                                                               NSError * _Nullable error))completion MTR_NEWLY_AVAILABLE;
+
 - (void)readAttributeGeneratedCommandListWithCompletion:(void (^)(NSArray * _Nullable value, NSError * _Nullable error))completion
     API_AVAILABLE(ios(16.4), macos(13.3), watchos(9.4), tvos(16.4));
 - (void)subscribeAttributeGeneratedCommandListWithParams:(MTRSubscribeParams *)params
--- src/darwin/Framework/CHIP/zap-generated/MTRBaseClusters.mm
+++ src/darwin/Framework/CHIP/zap-generated/MTRBaseClusters.mm
@@ -27988,6 +27988,50 @@ using chip::System::Clock::Timeout;
         });
 }
 
+- (void)readAttributeAverageWearCountWithCompletion:(void (^)(NSNumber * _Nullable value, NSError * _Nullable error))completion
+{
+    MTRReadParams * params = [[MTRReadParams alloc] init];
+    using TypeInfo = GeneralDiagnostics::Attributes::AverageWearCount::TypeInfo;
+    return MTRReadAttribute<MTRInt32uAttributeCallbackBridge, NSNumber, TypeInfo::DecodableType>(
+        params, completion, self.callbackQueue, self.device, self.endpoint, TypeInfo::GetClusterId(), TypeInfo::GetAttributeId());
+}
+
+- (void)subscribeAttributeAverageWearCountWithParams:(MTRSubscribeParams * _Nonnull)params
+                             subscriptionEstablished:(MTRSubscriptionEstablishedHandler _Nullable)subscriptionEstablished
+                                       reportHandler:(void (^)(NSNumber * _Nullable value, NSError * _Nullable error))reportHandler
+{
+    using TypeInfo = GeneralDiagnostics::Attributes::AverageWearCount::TypeInfo;
+    MTRSubscribeAttribute<MTRInt32uAttributeCallbackSubscriptionBridge, NSNumber, TypeInfo::DecodableType>(params,
+        subscriptionEstablished, reportHandler, self.callbackQueue, self.device, self.endpoint, TypeInfo::GetClusterId(),
+        TypeInfo::GetAttributeId());
+}
+
++ (void)readAttributeAverageWearCountWithClusterStateCache:(MTRClusterStateCacheContainer *)clusterStateCacheContainer
+                                                  endpoint:(NSNumber *)endpoint
+                                                     queue:(dispatch_queue_t)queue
+                                                completion:
+                                                    (void (^)(NSNumber * _Nullable value, NSError * _Nullable error))completion
+{
+    auto * bridge = new MTRInt32uAttributeCallbackBridge(queue, completion);
+    std::move(*bridge).DispatchLocalAction(
+        clusterStateCacheContainer.baseDevice, ^(Int32uAttributeCallback successCb, MTRErrorCallback failureCb) {
+            if (clusterStateCacheContainer.cppClusterStateCache) {
+                chip::app::ConcreteAttributePath path;
+                using TypeInfo = GeneralDiagnostics::Attributes::AverageWearCount::TypeInfo;
+                path.mEndpointId = static_cast<chip::EndpointId>([endpoint unsignedShortValue]);
+                path.mClusterId = TypeInfo::GetClusterId();
+                path.mAttributeId = TypeInfo::GetAttributeId();
+                TypeInfo::DecodableType value;
+                CHIP_ERROR err = clusterStateCacheContainer.cppClusterStateCache->Get<TypeInfo>(path, value);
+                if (err == CHIP_NO_ERROR) {
+                    successCb(bridge, value);
+                }
+                return err;
+            }
+            return CHIP_ERROR_NOT_FOUND;
+        });
+}
+
 - (void)readAttributeGeneratedCommandListWithCompletion:(void (^)(NSArray * _Nullable value, NSError * _Nullable error))completion
 {
     MTRReadParams * params = [[MTRReadParams alloc] init];
